5206331952c6379a718c56fdb46f8e94f1890b25
apsourg <apostolis@scio.systems>
2022-03-04 10:21:05 +0200

    Refactor redis fetch call

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,7 +26,7 @@ class AppServiceProvider extends ServiceProvider
     {
         //Calls to the cacheapi/retrievevalue endpoint at scio.services to retrieve the clarisa vocabularies
         Http::macro('redisFetch',function (){
-           $response =  Http::post(env('SCIO_REDIS_API',''), [
+           $response =  Http::post(env('SCIO_REDIS_API','').'/cacheapi/retrievevalue', [
                "key" => "clarisa_vocabularies",             //key needed for redis
            ]);
             return json_decode($response['response']);      //transform the data to json
